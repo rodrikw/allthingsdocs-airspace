d99759b9579f8b1662a4375566b8dba518581574
rodrikw <rodrik.wade@gmail.com>
2022-03-23 14:34:42 +0200

    Update start-hugo-server.ps1

--- start-hugo-server.ps1
+++ start-hugo-server.ps1
@@ -1,7 +1,7 @@
 # Run the server, including draft content:
 # & hugo server -D
 # Run the server without draft content:
-& hugo server -D
+& hugo server -D --destination public
 
 
 Write-Host "`r`nPress any key to continue..."
